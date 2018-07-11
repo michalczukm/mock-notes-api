cc39024c3756b8e4b2fc504e4b73f6a4006b4649
Michał Michalczuk <michalczukm@gmail.com>
2018-07-11 23:54:39 +0200

    now part of routes require auth

---
+++ auth.js
@@ -0,0 +1,32 @@
+// don't do it that way. Its only for demo purposes
+const HARDCODED_DEMO_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtaWNoYWxjenVrbS54eXoiLCJpYXQiOjE1MzEzNDY1MjEsImV4c' +
+    'CI6MTU5NDUwNDkyMSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiYWRtaW4uYWRtaW5AZXhhbXBsZS5jb20iLCJOYW1lIjoiQW' +
+    'RtaW4iLCJTdXJuYW1lIjoiQWRtaW4iLCJFbWFpbCI6ImFkbWluLmFkbWluQGV4YW1wbGUuY29tIiwiUm9sZSI6IlN5c3RlbSBBZG1pbnN0' +
+    'cmF0b3IifQ.q14fIWoyZsvkDmDDtE6Hi34zpmSJLNmtueFaHSd82YM';
+
+/**
+ * Returns hardcoded token for demo purposes
+ * @returns {string}
+ */
+const generateJWTToken = () => {
+    // check this token at https://jwt.io/#debugger-io?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtaWNoYWxjenVrbS54eXoiLCJpYXQiOjE1MzEzNDY1MjEsImV4cCI6MTU5NDUwNDkyMSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiYWRtaW4uYWRtaW5AZXhhbXBsZS5jb20iLCJOYW1lIjoiQWRtaW4iLCJTdXJuYW1lIjoiQWRtaW4iLCJFbWFpbCI6ImFkbWluLmFkbWluQGV4YW1wbGUuY29tIiwiUm9sZSI6IlN5c3RlbSBBZG1pbnN0cmF0b3IifQ.q14fIWoyZsvkDmDDtE6Hi34zpmSJLNmtueFaHSd82YM
+
+    const KEY = 'qwertyuiopasdfghjklzxcvbnm123456';
+    return HARDCODED_DEMO_TOKEN;
+};
+
+/**
+ * Only admin allowed ;) for demo purposes
+ * @param username
+ * @param password
+ * @returns {boolean}
+ */
+const isAuthenticated = (username, password) => username === 'admin' && password === 'admin';
+
+const isAuthorized = (request) => request.get('Authorization') === `Bearer ${HARDCODED_DEMO_TOKEN}`;
+
+module.exports = {
+    generateJWTToken,
+    isAuthorized,
+    isAuthenticated
+};
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const jsonServer = require('json-server');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const auth = require('./auth');
 
 const port = process.env.PORT || 3030;
 
@@ -8,8 +11,46 @@ const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(bodyParser.json());
+
+// handle adding createdOn date to all (not all has it but, oh well)
+server.use((request, respnse, next) => {
+    if (request.method === 'POST') {
+        request.body.createdOn = Date.now()
+    }
+
+    next();
+});
+
+server.post('/v2/login', (request, response) => {
+    const {username, password} = request.body;
+
+    if (auth.isAuthenticated(username, password)) {
+        response.json({
+            name: 'Admin Admin',
+            token: auth.generateJWTToken()
+        });
+    } else {
+        response.sendStatus(400);
+    }
+});
+
+/**
+ * Only v2 routes need authorization
+ */
+server.use(/^\/v2\/.*$/, (request, response, next) => {
+    if (auth.isAuthorized(request)) {
+        next();
+    } else {
+        response.sendStatus(401);
+    }
+});
+
+// v2 use same resources as not prefix version, but requires authorization
+server.use('/v2', router);
+
 server.use(router);
 
 server.listen(port, () => {
-  console.log('Server is running')
+    console.log('Server is running')
 });
